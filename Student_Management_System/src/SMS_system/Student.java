9bf239246cdad7f513375dcf81f0c2f8858bb987
Bharat <you@example.com>
2025-01-13 11:12:28 +0545

    Using getter setter.

--- Student_Management_System/src/SMS_system/Student.java
+++ Student_Management_System/src/SMS_system/Student.java
@@ -2,14 +2,25 @@ package SMS_system;
 
 
 public class Student {
-	String name;
-	int age;
-	public Student(String name, int age) {
-		this.name=name;
-		this.age=age;
+	private String name;
+	private int age;
+	public Student() {
 		
+
+	}
+	public void setage(int age) {
+		this.age = age;
+	}
+	public void setname(String name) {
+		this.name = name;
 	}
 	
+	public int getage() {
+		return age;
+	}
+	public String getname() {
+		return name;
+	}
 	
 
 }
